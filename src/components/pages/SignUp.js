806765279d942b8d0d2f5497e3ed504a4fad926e
Tariq555 <90894237+Tariq555@users.noreply.github.com>
2023-05-27 14:28:01 +0200

    Update

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  // Define state variables for form inputs
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Define an event handler to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send form data to server using fetch()
+    fetch('/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, email, password })
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('Signup successful');
+        } else {
+          console.error('Error signing up');
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='sign-up'>
+      <h1>Sign Up</h1>
+
+      <label htmlFor='name'>Name:</label>
+      <input type='text' id='name' name='name' value={name} onChange={(e) => setName(e.target.value)} required />
+
+      <label htmlFor='email'>Email:</label>
+      <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+
+      <label htmlFor='password'>Password:</label>
+      <input type='password' id='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+
+      <button type='submit'>Submit</button>
+    </form>
+  );
 }
