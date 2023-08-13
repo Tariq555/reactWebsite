9ed638f17acdaf6d7ebb51ac836d73adec6a1e37
Tariq555 <90894237+Tariq555@users.noreply.github.com>
2023-08-13 23:57:27 +0200

    test2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={s} />
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
     </>
