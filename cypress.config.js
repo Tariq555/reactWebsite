f1d426e5b5bce9875134597efdcc53054c0d263b
Tariq555 <90894237+Tariq555@users.noreply.github.com>
2023-12-08 14:54:24 +0100

    added cypress test

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ...rest of the Cypress project config
+  projectId: "wkdqzv",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
